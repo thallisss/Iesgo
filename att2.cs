7c6868930cb01b1b0f1fadb9e9e9db06eda6c1f9
thallisss <142070499+thallisss@users.noreply.github.com>
2024-08-12 17:21:29 -0300

    Add files via upload

---
+++ att2.cs
@@ -0,0 +1,17 @@
+﻿//Isto é um comentário
+//Diretiva com o NameSpace System
+
+// O NameSpace vai agrupar as bibliotecas de códigos para evitar problemas com nomes
+
+using System;
+using System.Text
+public class HelloWord
+
+{
+    public static void Main(string[] args) //Método Main - Ponto de entrada
+    {
+        // Acessível em qualquer lugar (Public)
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Olá, mundo!");
+    }
+}
